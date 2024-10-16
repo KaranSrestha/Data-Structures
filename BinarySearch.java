046db40a65705a26d5ec7212eeed431d38c0da2c
KaranSrestha <karansrestha@gmail.com>
2024-10-16 19:59:03 +0530

    code b search for both ascending and descending order

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,9 +1,10 @@
 public class BinarySearch {
+    //for ascending order
     public static int bSearch(int[] arr, int num){
         int left = 0;
         int right = arr.length-1;
         while(left <= right){
-            int mid = left + (right-left)/2;
+            int mid = left + (right-left)/2; //left+right may go out of range of integers
             if(arr[mid] == num){
                 return mid;
             }else if(arr[mid] < num){
@@ -16,10 +17,35 @@ public class BinarySearch {
         }
         return -1; //not found
     }
+    public static int orderAgnosticBS(int[] arr, int num){
+        int left = 0;
+        int right = arr.length - 1;
+
+        boolean isAscen = true;
+        if(arr[left] > arr[right]){
+            isAscen = false;
+        }
+
+        while(left <= right){
+            int mid = left + (right-left)/2; //left+right may go out of range of integers
+            if(arr[mid] == num){
+                return mid;
+            }else if(arr[mid] < num){
+                //shift the left pointer
+                if(isAscen) left = mid + 1;
+                else right = mid - 1;
+            }else{
+                //shift the right pointer
+                if(isAscen) right = mid - 1;
+                else left = mid + 1;
+            }
+        }
+        return -1;
+    }
     public static void main(String[] args) {
-        int[] arr = {1,3,1,2,43,5,7,10};
-        int num = 8;  //number to be searched
-        int i = bSearch(arr, num);
+        int[] arr = {1, 2, 3 ,4};
+        int num = 1;  //number to be searched
+        int i = orderAgnosticBS(arr, num);
         System.out.println(i);
     }
 }
